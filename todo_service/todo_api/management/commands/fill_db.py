0fdc05de5a6403f8e4bfc2bf4f7b6b7468055b1b
Sergey_Sukhanov <sergey.bv26@gmail.com>
2022-05-22 18:59:24 +0300

    Выполнено практическое задание к 6 уроку

--- todo_service/todo_api/management/commands/fill_db.py
+++ todo_service/todo_api/management/commands/fill_db.py
@@ -1,5 +1,4 @@
 from mimesis import Person
-from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 
 from todo_api.models import WebUser
@@ -7,15 +6,15 @@ from todo_api.models import WebUser
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        User.objects.all().delete()
-        User.objects.create_superuser('drf', password='geekbrains')
 
         WebUser.objects.all().delete()
+        WebUser.objects.create_superuser('drf', password='geekbrains')
         person = Person('ru')
         for num in range(20):
-            WebUser.objects.create(
+            WebUser.objects.create_user(
                 username=person.username(),
                 firstname=person.first_name(),
                 lastname=person.last_name(),
-                email=person.email())
+                email=person.email(),
+                password=person.password())
 
