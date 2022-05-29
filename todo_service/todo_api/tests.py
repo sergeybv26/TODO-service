b3a6aef6fdf69096b125634614f6f11632b6bbd2
sergeybv26 <sergey.bv26@gmail.com>
2022-05-29 23:55:22 +0300

    Start work

--- todo_service/todo_api/tests.py
+++ todo_service/todo_api/tests.py
@@ -1,3 +1,34 @@
+from mimesis import Person
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from todo_api.views import UserViewSet
+
+
+class TestUserViewSet(TestCase):
+    def test_get_user_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        person = Person('ru')
+        username = person.username()
+        firstname = person.first_name()
+        lastname = person.last_name()
+        email = person.email()
+        password = person.password()
+        request = factory.post('/api/users/', {
+            'username': username,
+            'firstname': firstname,
+            'lastname': lastname,
+            'email': email,
+            'password': password
+        }, format='json')
+        view = UserViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- todo_service/todo_api/views.py
+++ todo_service/todo_api/views.py
@@ -11,23 +11,3 @@ from todo_api.serializers import UserModelSerializer
 class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     queryset = WebUser.objects.all()
     serializer_class = UserModelSerializer
-
-    # def list(self, request):
-    #     users = WebUser.objects.all()
-    #     serializer = UserModelSerializer(users, many=True)
-    #     return Response(serializer.data)
-    #
-    # def retrieve(self, request, pk=None):
-    #     user = get_object_or_404(WebUser, pk=pk)
-    #     serializer = UserModelSerializer(user)
-    #     return Response(serializer.data)
-    #
-    # def update(self, request, pk=None):
-    #     user = get_object_or_404(WebUser, pk=pk)
-    #     serializer = UserModelSerializer(user)
-    #     return Response(serializer.data)
-    #
-    # def partial_update(self, request, pk=None):
-    #     user = get_object_or_404(WebUser, pk=pk)
-    #     serializer = UserModelSerializer(user)
-    #     return Response(serializer.data)
