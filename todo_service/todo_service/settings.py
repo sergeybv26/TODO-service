42777b9049843d9abcdceaf99e29c9a9aee7fbcc
sergeybv26 <sergey.bv26@gmail.com>
2022-06-02 16:33:07 +0300

    Completed versioning

--- todo_service/todo_service/settings.py
+++ todo_service/todo_service/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
@@ -180,6 +181,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 }
 
 AUTH_USER_MODEL = 'todo_api.WebUser'
--- todo_service/todo_service/urls.py
+++ todo_service/todo_service/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -32,6 +33,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token),
     path('api/', include(router.urls)),
+    re_path(r'^api/(?P<version>\d\.\d\.\d)/users/$', UserViewSet.as_view({'get': 'list'})),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView, name='token_refresh'),
 ]
