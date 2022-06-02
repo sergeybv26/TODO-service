72d6fbd55b3ad5871966a71950d7d5dfc55012d0
sergeybv26 <sergey.bv26@gmail.com>
2022-06-02 16:42:46 +0300

    Completed task 9

--- todo_service/todo_service/urls.py
+++ todo_service/todo_service/urls.py
@@ -15,13 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from todo_api.views import UserViewSet
 from todo_work.views import ProjectModelViewSet, ToDoModelViewSet
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='TODO API',
+        default_version='0.0.1',
+        description='Documentation to project TODO API',
+        contact=openapi.Contact(email='user@test.ru'),
+        license=openapi.License(name='MIT license'),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('users', UserViewSet, basename='users')
@@ -36,4 +50,7 @@ urlpatterns = [
     re_path(r'^api/(?P<version>\d\.\d\.\d)/users/$', UserViewSet.as_view({'get': 'list'})),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView, name='token_refresh'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
